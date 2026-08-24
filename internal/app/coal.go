af490200787249fab2fcde470e8b78e843a31128
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-24 19:44:34 +0800

    init: paperweb

---
+++ internal/app/coal.go
@@ -0,0 +1,91 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/lacsar712/paperweb/internal/clock"
+	"github.com/lacsar712/paperweb/internal/model"
+)
+
+func (a *App) advanceClock(d time.Duration) {
+	if mc, ok := a.clk.(*clock.ManualClock); ok {
+		mc.Advance(d)
+		time.Sleep(time.Millisecond)
+	} else {
+		time.Sleep(d)
+	}
+}
+
+var activeFiberCancel context.CancelFunc
+
+func (a *App) bindFiberLoop(holder string, ctx context.Context) context.Context {
+	a.mu.Lock()
+	if activeFiberCancel != nil {
+		activeFiberCancel()
+	}
+	child, cancel := context.WithCancel(ctx)
+	activeFiberCancel = cancel
+	a.mu.Unlock()
+	return child
+}
+
+func (a *App) cancelFiberLoop(holder string) {
+	a.mu.Lock()
+	if activeFiberCancel != nil {
+		activeFiberCancel()
+		activeFiberCancel = nil
+	}
+	a.mu.Unlock()
+}
+
+func (a *App) cancelAllFiberLoops() {
+	a.mu.Lock()
+	for holder, cancel := range a.fiberLoopCancels {
+		cancel()
+		delete(a.fiberLoopCancels, holder)
+	}
+	a.mu.Unlock()
+}
+
+func (a *App) CoalFeedTPH() float64 {
+	return a.Snapshot().Headbox.FiberFlowTPH
+}
+
+func (a *App) RunFiberRamp(ctx context.Context, holder string, targetTPH float64) error {
+	loopCtx := a.bindFiberLoop(holder, ctx)
+	defer a.cancelFiberLoop(holder)
+	for {
+		if err := loopCtx.Err(); err != nil {
+			return fmt.Errorf("%w", model.ErrContextDone)
+		}
+		snap := a.Snapshot()
+		current := snap.Headbox.FiberFlowTPH
+		if current >= targetTPH {
+			return nil
+		}
+		comb := snap.Headbox
+		comb.FiberFlowTPH = current + 1.0
+		_ = a.store.UpdateHeadbox(a.cfg.UnitID, comb)
+		a.telemetry.RecordCoalFeed(comb.FiberFlowTPH)
+		a.advanceClock(100 * time.Millisecond)
+	}
+}
+
+func (a *App) RunCoalFeed(ctx context.Context, holder string, steps int) error {
+	loopCtx := a.bindFiberLoop(holder, ctx)
+	defer a.cancelFiberLoop(holder)
+	for i := 0; steps <= 0 || i < steps; i++ {
+		if err := loopCtx.Err(); err != nil {
+			return fmt.Errorf("%w", model.ErrContextDone)
+		}
+		snap := a.Snapshot()
+		comb := snap.Headbox
+		comb.FiberFlowTPH += 0.5
+		_ = a.store.UpdateHeadbox(a.cfg.UnitID, comb)
+		a.telemetry.RecordCoalFeed(comb.FiberFlowTPH)
+		a.advanceClock(100 * time.Millisecond)
+	}
+	return nil
+}
